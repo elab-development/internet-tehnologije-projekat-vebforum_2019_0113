c860a0d66a087df14a8531211bd454d8f4d8ca81
an20190113 <an20190113@student.fon.bg.ac.rs>
2024-01-20 00:53:28 +0100

    omoguceno dodavanje komentara za odredjene psotove u lokalnoj memoriji

--- webforumfront/src/App.js
+++ webforumfront/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ForumHomepage from './components/home/ForumHomePage';
@@ -8,14 +8,15 @@ import ObjaveComponent from './components/objave/ObjaveComponent ';
 import Details from './components/objave/Details';
 
 function App() {
+  const [comments, setComments]= useState([]);
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<ForumHomepage />} />
-          <Route path="/objave/:id" element={<Details />} />
-          <Route path="/objave" element={<ObjaveComponent />} />
+          <Route path="/objave/:id" element={<Details comments={comments}  setComments={setComments}/>} />
+          <Route path="/objave" element={<ObjaveComponent  />} />
         </Routes>
       </div>
     </BrowserRouter>
--- webforumfront/src/components/objave/Details.jsx
+++ webforumfront/src/components/objave/Details.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './Details.css'; 
-import { FaThumbsUp } from "react-icons/fa";
-import { FaThumbsDown } from "react-icons/fa";
+import './Details.css';
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 
-const Details = () => {
+const Details = ({comments, setComments}) => {
   const { id } = useParams();
   const [objava, setObjava] = useState(null);
+  
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
     axios.get(`http://127.0.0.1:8000/api/objave/${id}`)
@@ -17,13 +18,27 @@ const Details = () => {
       .catch(error => console.error('Error fetching data:', error));
   }, [id]);
 
+  const handleNewCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const submitComment = () => {
+    const comment = {
+      text: newComment,
+      date: new Date().toISOString(),
+      objavaId: id
+    };
+    setComments([...comments, comment]);
+    setNewComment(''); // Reset the comment input field
+  };
+
   if (!objava) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="details-container">
-      <div className="details-header" style={{ backgroundColor: '#yourColor' }}>
+           <div className="details-header" style={{ backgroundColor: '#yourColor' }}>
         <h2 className="details-title">{objava.naziv}</h2>
         <span className="details-category">{objava.tema.naziv_teme}</span>
         <p className="details-date">{objava.datum_objave}</p>
@@ -46,6 +61,23 @@ const Details = () => {
           <FaThumbsDown style={{ color: 'red' }} />
           {objava.broj_nesvidjanja}
         </strong>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Dodaj komentar..."
+          value={newComment}
+          onChange={handleNewCommentChange}
+        />
+        <button onClick={submitComment}>Postavi komentar</button>
+      </div>
+      <div>
+        {comments.map((comment, index) => (
+          <div key={index} className="comment">
+            <p>{comment.text}</p>
+            <p>{comment.date}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
