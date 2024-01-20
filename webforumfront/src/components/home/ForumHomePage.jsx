b075458b8446b6cfe656c7f3d96de5b2796280ab
an20190113 <an20190113@student.fon.bg.ac.rs>
2024-01-20 01:28:34 +0100

    dopunjena pocetna stranica

--- webforumfront/src/components/home/ForumHomePage.jsx
+++ webforumfront/src/components/home/ForumHomePage.jsx
@@ -37,26 +37,37 @@ const ForumHomePage = () => {
 
   return (
     <div className="forum-homepage">
-    <header className="header">
-      <h1>Welcome to the Forum!</h1>
-      <p>Join the conversation and share your thoughts!</p>
-      <div className="icons">
-        <i className="fas fa-comments"></i> {/* Chat icon */}
-        <i className="fas fa-smile"></i> {/* Smile icon */}
-        <i className="fas fa-users"></i> {/* Users icon */}
+      <header className="header">
+        <h1>Welcome to the Forum!</h1>
+        <p>Join the conversation and share your thoughts!</p>
+        <div className="icons">
+          <i className="fas fa-comments"></i> {/* Chat icon */}
+          <i className="fas fa-smile"></i> {/* Smile icon */}
+          <i className="fas fa-users"></i> {/* Users icon */}
+        </div>
+      </header>
+      <div className="content">
+        <div className="quote">
+          <h2>Quote of the Day</h2>
+          <p>{quote}</p>
+        </div>
+        <div className="joke">
+          <h2>Joke of the Day</h2>
+          <p>{joke}</p>
+        </div>
       </div>
-    </header>
-    <div className="content">
-        
-      <div className="quote">
-        <h2>Quote of the Day</h2>
-        <p>{quote}</p>
+      <div className="rules-section">
+        <h2>Pravila Ponašanja na Forumu</h2>
+        <p>Dobrodošli u našu zajednicu! Molimo vas da se pridržavate sledećih pravila:</p>
+        <ul>
+          <li>Bez vređanja ili omalovažavanja drugih korisnika.</li>
+          <li>Nema širenja govora mržnje ili diskriminacije bilo koje vrste.</li>
+          <li>Budite ljubazni i poštujte mišljenja drugih.</li>
+          <li>Ne širite lažne informacije ili sadržaje koji krše autorska prava.</li>
+        </ul>
+        <p>Zajedno možemo stvoriti prijateljsko i podržavajuće okruženje!</p>
       </div>
-      <div className="joke">
-        <h2>Joke of the Day</h2>
-        <p>{joke}</p>
-      </div>
-    </div> </div>
+    </div>
   );
 };
 
