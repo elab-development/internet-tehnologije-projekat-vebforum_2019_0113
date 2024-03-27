ab8e92641a9548521c1829e12ab3db4b522aa8bd
IvanaJ8 <ivana.jankoviic@gmail.com>
2024-03-27 16:07:51 +0100

    omoguceno azuriranje komentara

--- webforumfront/src/components/objave/CommentComponent .jsx
+++ webforumfront/src/components/objave/CommentComponent .jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
-const CommentComponent = ({ comment,deleteComment }) => {
+const CommentComponent = ({ comment, deleteComment }) => {
+  const user = JSON.parse(sessionStorage.getItem('user')); // Trenutni ulogovani korisnik
 
-  const user = JSON.parse(sessionStorage.getItem('user')); //trenutni ulogovani korisnik, izvlacimo ovaj podatak da bismo mogli da omogucimo brisanje i izmenu komentara koje je kreirao sam korisnik
+  const [isEditing, setIsEditing] = useState(false); // Stanje koje označava da li se komentar trenutno ažurira
+  const [updatedText, setUpdatedText] = useState(comment.tekst_komentara); // Tekst komentara koji se ažurira
 
   // Convert ISO string to Date object
   const dateObj = new Date(comment.datum_komentarisanja);
@@ -17,30 +19,67 @@ const CommentComponent = ({ comment,deleteComment }) => {
     minute: '2-digit', // "00", "01", etc.
     hour12: true // Use 12-hour format with AM/PM
   });
-    // Function to handle delete comment
-    const handleDelete = async () => {
-      try {
-        const token = sessionStorage.getItem('token'); // Dobijanje tokena iz sessionStorage-a
-    
-        await axios.delete(`http://127.0.0.1:8000/api/komentari/${comment.id_komentara}`, {
-          headers: {
-            'Authorization': `Bearer ${token}` // Dodavanje tokena u zaglavlju
-          }
-        });
-    
-        // Callback to parent to update comments
-        deleteComment(comment.id_komentara); //fja brise komentar iz lokalne memorije
-      } catch (error) {
-        console.error('Error deleting comment:', error);
-      }
-    };
-    
+
+  // Function to handle delete comment
+  const handleDelete = async () => {
+    try {
+      const token = sessionStorage.getItem('token'); // Dobijanje tokena iz sessionStorage-a
+
+      await axios.delete(`http://127.0.0.1:8000/api/komentari/${comment.id_komentara}`, {
+        headers: {
+          'Authorization': `Bearer ${token}` // Dodavanje tokena u zaglavlju
+        }
+      });
+
+      // Callback to parent to update comments
+      deleteComment(comment.id_komentara); // Funkcija briše komentar iz lokalne memorije
+    } catch (error) {
+      console.error('Error deleting comment:', error);
+    }
+  };
+
+  // Function to handle update comment
+  const handleUpdate = async () => {
+    try {
+      const token = sessionStorage.getItem('token'); // Dobijanje tokena iz sessionStorage-a
+
+      await axios.patch(`http://127.0.0.1:8000/api/komentari/izmeniTekst/${comment.id_komentara}`, {
+        tekst: updatedText // Novi tekst komentara
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}` // Dodavanje tokena u zaglavlju
+        }
+      });
+
+      // Update the comment text locally
+      comment.tekst_komentara = updatedText;
+      setIsEditing(false); // Završi ažuriranje komentara
+    } catch (error) {
+      console.error('Error updating comment:', error);
+    }
+  };
+
   return (
     <div className="comment">
       <span className="comment-date">{formattedDate}</span>
-      <span className="comment-text">{comment.tekst_komentara}</span>
+      {isEditing ? ( // Ako se komentar ažurira, prikaži polje za unos teksta
+        <input 
+          type="text" 
+          value={updatedText} 
+          onChange={(e) => setUpdatedText(e.target.value)} 
+        />
+      ) : (
+        <span className="comment-text">{comment.tekst_komentara}</span>
+      )}
       {comment.user.id_korisnika === user.id && ( // Check if comment was created by current user
-        <button onClick={handleDelete}>Obriši komentar</button>
+        <>
+          {isEditing ? ( // Ako se komentar ažurira, prikaži dugme za završetak ažuriranja
+            <button onClick={handleUpdate}>Završi ažuriranje</button>
+          ) : (
+            <button onClick={() => setIsEditing(true)}>Ažuriraj komentar</button>
+          )}
+          <button onClick={handleDelete}>Obriši komentar</button>
+        </>
       )}
     </div>
   );
