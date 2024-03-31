08b2db030224504e29b1d5be049c804af4f6e6a8
IvanaJ8 <ivana.jankoviic@gmail.com>
2024-03-31 15:30:33 +0200

    dodati filteri za objave po temama

--- webforumfront/src/components/objave/ObjaveComponent .jsx
+++ webforumfront/src/components/objave/ObjaveComponent .jsx
@@ -3,13 +3,16 @@ import axios from 'axios';
 import ObjavaComponent from './ObjavaComponent';
 import './ObjaveComponent.css';
 import useObjave from './useObjave';
+import useTeme from './useTeme'; 
 
 const ObjaveComponent = () => {
-  const [objave, setObjave] =  useObjave('http://127.0.0.1:8000/api/objave');
+  const [objave, setObjave] = useObjave('http://127.0.0.1:8000/api/objave');
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedTema, setSelectedTema] = useState(''); 
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(5);  
-
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const { teme, isLoading, error } = useTeme('http://127.0.0.1:8000/api/teme'); 
+  console.log(teme)
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/objave')
       .then(response => {
@@ -20,14 +23,34 @@ const ObjaveComponent = () => {
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
-    setCurrentPage(1); // Reset to first page on search
+    setCurrentPage(1); // Resetujemo na prvu stranicu pri pretrazi
   };
 
-  const filteredObjave = objave.filter(objava =>
-    objava.naziv.toLowerCase().includes(searchTerm.toLowerCase())
-    || objava.tekst.toLowerCase().includes(searchTerm.toLowerCase())
+  const handleTemaChange = (e) => {
+    setSelectedTema(e.target.value);
+    console.log(selectedTema)
+    console.log(e.target.value)
+    setSearchTerm(''); // Resetujemo pretragu pri promeni teme
+    setCurrentPage(1); // Resetujemo na prvu stranicu pri promeni teme
+  };
+
+
+  // Prvo filtriramo po nazivu
+  let filteredObjave = objave.filter(objava =>
+    objava.naziv.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    objava.tekst.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  // Zatim filtriramo rezultate po temi ako je odabrana tema
+  if (selectedTema !== '') {
+    const filteredByTema = [];
+    for (let i = 0; i < objave.length; i++) {
+      if (parseInt(selectedTema) === objave[i].tema.id_teme) {
+        filteredByTema.push(objave[i]);
+      }
+    }
+    filteredObjave = filteredByTema;
+  } 
   // Pagination logic
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -42,9 +65,17 @@ const ObjaveComponent = () => {
 
   return (
     <div className="objave-container">
-      <input 
-        type="text" 
-        placeholder="Pretraži objave..." 
+     
+      <select value={selectedTema} onChange={handleTemaChange} className="teme-select">
+        <option value="">Sve teme</option>
+        {teme.map(tema => (
+          <option key={tema.id_teme} value={tema.id_teme}>{tema.naziv_teme}</option>
+        ))}
+      </select>
+
+      <input
+        type="text"
+        placeholder="Pretraži objave..."
         value={searchTerm}
         onChange={handleSearchChange}
         className="search-input"
